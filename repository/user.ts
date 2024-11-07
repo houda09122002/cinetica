923a3cfe47d62af6515bceb7f0fcd894d500e46f
Houda <houdakhiati97@gmail.com>
2024-11-07 18:24:53 +0100

    main page

--- repository/user.ts
+++ repository/user.ts
@@ -1,6 +1,6 @@
 // repository/user.ts
 
 export const user = {
-    username: "Houda Yasmine",  // Remplacez par le nom d'utilisateur de votre choix
-    password: "$2b$10$eFBu0VJBucHTSUfsdBW3tOwNG/5Cs24OE1wxtQpuU5rvZbxvWwTIS"  // Remplacez par votre mot de passe encrypté avec bcrypt.fr
+    username: "Houda Yasmine",  
+    password: "$2b$10$eFBu0VJBucHTSUfsdBW3tOwNG/5Cs24OE1wxtQpuU5rvZbxvWwTIS"  
 };
