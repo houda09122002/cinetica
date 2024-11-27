3b7d6f16d6cf773fcd911001f7e672cadf0792b9
Houda <houdakhiati97@gmail.com>
2024-11-27 15:21:44 +0100

    fix bug

--- app/main/shows/layout.tsx
+++ app/main/shows/layout.tsx
@@ -1,27 +1,12 @@
 "use client"
 
-import { usePathname } from 'next/navigation'
+
 
 export default function ShowsLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const pathname = usePathname()
-
-  // Fonction pour obtenir le titre selon le chemin
-  const getTitle = () => {
-    switch (pathname) {
-      case '/main/shows/on-the-air':
-        return 'On The Air Shows'
-      case '/main/shows/popular':
-        return 'Popular Shows'
-      case '/main/shows/top-rated':
-        return 'Top Rated Shows'
-      default:
-        return 'TV Shows'
-    }
-  }
 
   return (
     <div>
