42bb28f596d7e2596aa127100600203b15c995f7
Houda <houdakhiati97@gmail.com>
2024-11-27 14:48:24 +0100

    Movies

--- app/main/movies/layout.tsx
+++ app/main/movies/layout.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
 import { usePathname } from 'next/navigation'
 
 export default function MoviesLayout({
@@ -10,7 +11,19 @@ export default function MoviesLayout({
 }) {
   const pathname = usePathname()
 
-
+  // Fonction pour obtenir le titre selon le chemin
+  const getTitle = () => {
+    switch (pathname) {
+      case '/main/movies/now-playing':
+        return 'Now Playing Movies'
+      case '/main/movies/popular':
+        return 'Popular Movies'
+      case '/main/movies/top-rated':
+        return 'Top Rated Movies'
+      default:
+        return 'Movies'
+    }
+  }
 
   return (
     <div>
